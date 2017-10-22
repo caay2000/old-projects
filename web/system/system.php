4fc110b35d5a727cd413a05cc2d702958858b648
caay2000 <caay2000@CAAY-PC>
2017-10-22 23:14:50 +0200

    * removing unneeded files

--- web/system/system.php
+++ web/system/system.php
@@ -4,9 +4,7 @@
 	require_once ('Controller.php');
 	require_once ('Locales.php');
 	require_once ('Service.php');
-	require_once ('AuthUtil.php');
-	if(DB_ACTIVE !== FALSE) include_once ('Database.php');
-	
+
 	define('DEFAULT_CONTROLLER_SYS', ucfirst(strtolower(DEFAULT_CONTROLLER)));
 	define('DEFAULT_CONTROLLER_METHOD_SYS', strtolower(DEFAULT_CONTROLLER_METHOD));
 	
